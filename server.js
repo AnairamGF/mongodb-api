9ece5aa91219c00bb256dc1e9c91a706f1ef6f72
Anairam Falcon <georgina_gf@live.com.mx>
2023-04-18 00:22:40 -0600

    server.js modified

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const User = require('./api/users');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use('/api/users', User);
 
 mongoose.connect(
     "mongodb://127.0.0.1/users",
