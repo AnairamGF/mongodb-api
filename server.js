5bea002fad3fc8f77f7926e7b31f04850e6d9144
Anairam Falcon <georgina_gf@live.com.mx>
2023-04-17 23:59:13 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+mongoose.connect(
+    "mongodb://127.0.0.1/users",
+    { useNewUrlParser: true }
+).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */ 
+            app.listen(4000, ()=>{
+                console.log('Server running on http://127.0.0.1:4000');
+            })
+    },
+    err => { /** handle initial connection error */ 
+            err & console.log(err) & console.log('Error connecting to db');
+    }
+  );
